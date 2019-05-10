24750886649a3dc989cb6e525e1a6cb95cc2ba6a
Hank Lancaster <tankcaster@gmail.com>
2019-05-10 03:20:45 +0000

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from './styled/styledNav';
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1>Wunga Bunga</h1>
+            <Button>Hello</Button>
+        </nav>
+    );
+}
+
+export default Nav;
